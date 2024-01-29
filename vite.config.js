6ec206b279279eb27c8f6dfa4cd83aa0f56988b0
wayne <qleonnex@gmail.com>
2024-01-29 14:24:38 +0500

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://qleonnex.github.io/",
+  base: "/todo/",
   plugins: [react()],
 })
